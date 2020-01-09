6e4bf57c4c36caf156fb8e69043538fac2cf857c
dev-93 <cjb940103@gmail.com>
2020-01-10 02:01:16 +0900

    Join 부분

---
+++ project/src/pages/Join/joincontainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from "../Nav/nav";
+import JoinContents from "./template/joincontents";
+
+function JoinContainer() {
+  return (
+    <>
+      <Nav />
+      <JoinContents />
+    </>
+  );
+}
+ 
+export default JoinContainer;
---
+++ project/src/pages/Join/template/joincontents.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/join.css"
+
+function JoinContents(){
+  return(
+    <>
+      <div className="wrap">
+        회원가입 컨텐츠
+      </div>
+    </>
+  )
+}
+
+export default JoinContents;
