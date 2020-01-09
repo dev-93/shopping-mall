f040f09a838d1b05c05728b41b389e98ad3401cd
dev-93 <cjb940103@gmail.com>
2020-01-10 01:47:18 +0900

    App.js 수정

--- project/src/App.js
+++ project/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './common/css/common.css';
+import './common/css/library.css';
+import MainContainer from "./pages/Main/maincontainer";
+import LoginContainer from "./pages/Login/logincontainer";
+import JoinContainer from "./pages/Join/joincontainer";
+import CartContainer from "./pages/Cart/cartcontainer";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+      return (
+          <>
+            <Router>
+              <Route exact path="/Main" component={MainContainer}/>
+              <Route path="/Login" component={LoginContainer}/>
+              <Route path="/Join" component={JoinContainer}/>
+              <Route path="/Cart" component={CartContainer}/>
+            </Router>
+          </>
+      );
+  }
 }
 
 export default App;
