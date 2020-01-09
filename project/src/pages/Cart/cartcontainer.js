1d45d5a9bc3f5ef69b41ec7971046d9304e67d76
dev-93 <cjb940103@gmail.com>
2020-01-10 02:00:36 +0900

    Cart 부분

---
+++ project/src/pages/Cart/cartcontainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from "../Nav/nav";
+import CartContents from "./template/cartcontents";
+
+function CartContainer() {
+  return (
+    <>
+      <Nav />
+      <CartContents />
+    </>
+  );
+}
+ 
+export default CartContainer;
---
+++ project/src/pages/Cart/template/cartcontents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../css/cart.css"
+
+function CartContents() {
+  return (
+    <>
+      <div className="wrap">
+        장바구니 컨텐츠
+      </div>
+    </>
+  );
+}
+ 
+export default CartContents;
