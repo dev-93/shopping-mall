cabc116c1c6b3f2ffbc7e0d1250c8393730215bc
dev-93 <cjb940103@gmail.com>
2020-01-10 02:01:27 +0900

    Login 부분

---
+++ project/src/pages/Login/logincontainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../Nav/nav";
+import LoginContents from "./template/logincontents";
+
+function LoginContainer(){
+  return(
+    <>
+      <Nav />
+      <LoginContents />
+    </>
+  )
+}
+
+export default LoginContainer;
---
+++ project/src/pages/Login/template/logincontents.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoginContents(){
+  return(
+    <>
+      <div className="wrap">
+        로그인 컨텐츠
+      </div>
+    </>
+  )
+}
+
+export default LoginContents;
