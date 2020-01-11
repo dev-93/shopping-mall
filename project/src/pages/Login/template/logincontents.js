a20fcc874579a669ba524e0a4af9f117a1469a57
dev-93 <cjb940103@gmail.com>
2020-01-11 18:22:13 +0900

    Login 화면 퍼블리싱 완료

--- project/src/pages/Login/template/logincontents.js
+++ project/src/pages/Login/template/logincontents.js
@@ -1,10 +1,27 @@
 import React from "react";
+import "../css/logincontents.css";
+import {Link} from "react-router-dom";
 
 function LoginContents(){
   return(
     <>
-      <div className="wrap">
-        로그인 컨텐츠
+      <div className="wrap lg_contents">
+        <div className="container">
+          <h3 className="title">Login</h3>
+          <div className="lg-box">
+            <input type="text" placeholder="ID"/>
+            <input type="password" placeholder="PASSWORD"/>
+          </div>
+
+          <div className="bt-box">
+            <Link to="/">
+              Sign In
+            </Link>
+            <Link to="/join">
+              Sign Up
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   )
