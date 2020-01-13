8084c9d9aac75923e8d5b12cf3cccf352464d81e
dev-93 <cjb940103@gmail.com>
2020-01-14 01:29:59 +0900

    카트가 비었을때 퍼블리싱 완료

--- project/src/pages/Cart/template/cartcontents.js
+++ project/src/pages/Cart/template/cartcontents.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import "../css/cart.css"
+import "../css/cartcontents.css"
 
 function CartContents() {
   return (
     <>
-      <div className="wrap">
-        장바구니 컨텐츠
+      <div className="wrap ca_contents">
+        <div className="contents">
+          <h3 className="title">CART</h3>
+          <div className="container">
+            <div className="tab_box">
+              <button type="button" className="tab domestic">국내배송상품(<span className="num">0</span>)</button>
+              <button type="button" className="tab foreign">해외배송상품(<span className="num">0</span>)</button>
+            </div>
+            
+            <div className="cart_box">
+              <div className="empty_cart">장바구니가 비어 있습니다.</div>
+            </div>
+
+            <div className="bt_box">
+              <button type="button">전체상품주문</button>
+              <button type="button">선택상품주문</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
