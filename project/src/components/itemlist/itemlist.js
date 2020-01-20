2fb98ef0d354620be972733ebe152818f1e8a2a0
dev-93 <cjb940103@gmail.com>
2020-01-21 01:26:44 +0900

    ItemList 데이터 보관

---
+++ project/src/components/itemlist/itemlist.js
@@ -0,0 +1,48 @@
+
+
+const ItemList = [
+  {
+    id:1,
+    name: "클랫 밴딩 와이드 생지데님",
+    image: "http://new-more.co.kr/web/product/big/20200115/00f8314ba9c9e283e751cf926327ea11.webp",
+    price: "37,000",
+    details: "트렌디하고 내츄럴한 무드의 밴딩 와이드 생지데님",
+  },
+  {
+    id:2,
+    name: "메린 루즈핏 싱글수트",
+    image: "http://new-more.co.kr/web/product/big/20200115/821c7c2842a2de5f81dac310cff6f66f.webp",
+    price: "75,000",
+    details: "부담없이, 멋스럽고 내츄럴한 와이드 핏라인의 슬랙스.",
+  },
+  {
+    id:3,
+    name: "메린 밴딩 와이드 슬랙스",
+    image: "http://new-more.co.kr/web/product/big/20200115/0ac24c35ea384bab57bc3aa16ca1cdf6.webp",
+    price: "29,000",
+    details: "트렌디하고 내츄럴한 루즈핏 라인의 싱글수트.",
+  },
+  {
+    id:4,
+    name: "세이디 오버핏 라이더 자켓",
+    image: "http://new-more.co.kr/web/product/big/201910/5ddaa0bb27cf1c627854a047a54da3a0.gif",
+    price: "59,000",
+    details: "기획특가, 트렌디하게 착용하기 좋은 오버핏 라이더자켓",
+  },
+  {
+    id:5,
+    name: "베이즈 프리미엄 니트지 긴팔티",
+    image: "http://new-more.co.kr/web/product/big/20200113/636498baaef517566208f3fe6d16cfec.webp",
+    price: "19,900",
+    details: "트렌디하고 내츄럴한 루즈핏 라인의 긴팔티",
+  },
+  {
+    id:6,
+    name: "메린 루즈핏 싱글자켓",
+    image: "http://new-more.co.kr/web/product/big/20200113/0dd80d16494816b25b6fed56f619b2a3.webp",
+    price: "69,900",
+    details: "군더더기 없는 깔끔한 디자인의 루즈핏 싱글자켓.",
+  },
+]
+
+export default ItemList;
