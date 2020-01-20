a79d1e92b1d16c06cd7b1e6aa0afae4813c16ba5
dev-93 <cjb940103@gmail.com>
2020-01-21 01:29:29 +0900

    ItemList 삭제 후 id값 전달

--- project/src/pages/Main/template/maincontents.js
+++ project/src/pages/Main/template/maincontents.js
@@ -3,12 +3,16 @@ import "../css/maincontents.css";
 import Footer from "../../Footer/footer";
 import Fade from "../../../components/slider/slider";
 import {Link} from "react-router-dom";
+import ItemList from "../../../components/itemlist/itemlist";
 
 
-function Item({name, image, price, details}) {
+function Item({id, name, image, price, details}) {
   return (
     <li className="item">
-      <Link to="/item" className="cloth-box">
+      <Link to={{
+        pathname: `/item/${id}`
+        }}
+      className="cloth-box">
         <img src={image} alt={name}/>
       </Link>
       <div className="detail-box">
@@ -16,56 +20,11 @@ function Item({name, image, price, details}) {
         <p className="price"><span className="num">{price}</span>원</p>
         <p className="details">{details}</p>
       </div>
-      <input type="checkbox" className="hide" id="check-1" />
-      <label htmlFor="check-1"></label>
+      <input type="checkbox" className="hide" id={`check-${id}`} />
+      <label htmlFor={`check-${id}`}></label>
     </li>
   )
 }
-
-const ItemList = [
-  {
-    id:1,
-    name: "클랫 밴딩 와이드 생지데님",
-    image: "http://new-more.co.kr/web/product/big/20200115/00f8314ba9c9e283e751cf926327ea11.webp",
-    price: "37,000",
-    details: "트렌디하고 내츄럴한 무드의 밴딩 와이드 생지데님",
-  },
-  {
-    id:2,
-    name: "메린 루즈핏 싱글수트",
-    image: "http://new-more.co.kr/web/product/big/20200115/821c7c2842a2de5f81dac310cff6f66f.webp",
-    price: "75,000",
-    details: "부담없이, 멋스럽고 내츄럴한 와이드 핏라인의 슬랙스.",
-  },
-  {
-    id:3,
-    name: "메린 밴딩 와이드 슬랙스",
-    image: "http://new-more.co.kr/web/product/big/20200115/0ac24c35ea384bab57bc3aa16ca1cdf6.webp",
-    price: "29,000",
-    details: "트렌디하고 내츄럴한 루즈핏 라인의 싱글수트.",
-  },
-  {
-    id:4,
-    name: "세이디 오버핏 라이더 자켓",
-    image: "http://new-more.co.kr/web/product/big/201910/5ddaa0bb27cf1c627854a047a54da3a0.gif",
-    price: "59,000",
-    details: "기획특가, 트렌디하게 착용하기 좋은 오버핏 라이더자켓",
-  },
-  {
-    id:5,
-    name: "베이즈 프리미엄 니트지 긴팔티",
-    image: "http://new-more.co.kr/web/product/big/20200113/636498baaef517566208f3fe6d16cfec.webp",
-    price: "19,900",
-    details: "트렌디하고 내츄럴한 루즈핏 라인의 긴팔티",
-  },
-  {
-    id:6,
-    name: "메린 루즈핏 싱글자켓",
-    image: "http://new-more.co.kr/web/product/big/20200113/0dd80d16494816b25b6fed56f619b2a3.webp",
-    price: "69,900",
-    details: "군더더기 없는 깔끔한 디자인의 루즈핏 싱글자켓.",
-  },
-]
 
 function MainContents(){
   return (
@@ -86,6 +45,7 @@ function MainContents(){
                 image={cloth.image}
                 price={cloth.price}
                 details={cloth.details}
+                id={cloth.id}
               />
             ))}
           </ul>
