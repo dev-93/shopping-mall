11c92ea2511972cff6d0c8d68d2a1f630a07baef
dev-93 <cjb940103@gmail.com>
2020-01-27 02:18:51 +0900

    로그인 id값과 pw값 기존LS에 있는 값과 비교하여 로그인하기 개발진행 중

--- project/src/pages/Login/template/logincontents.js
+++ project/src/pages/Login/template/logincontents.js
@@ -3,21 +3,61 @@ import "../css/logincontents.css";
 import {Link} from "react-router-dom";
 
 function LoginContents(){
+
+  function getInfo(){
+    const USER_INFO = "userInfo";
+    
+    const loadInfo = localStorage.getItem(USER_INFO);
+    const parseloadInfo = JSON.parse(loadInfo);
+
+    let i;
+    let InfoLength = parseloadInfo.length;
+
+    for (i=0; i < InfoLength; i++ ){
+      console.log(parseloadInfo[i].Id);
+      console.log(parseloadInfo[i].Pw);
+    };
+  }
+
+  function checkPw(){
+    const loginPw = document.querySelector(".login-pw");
+    const loginPwValue = loginPw.value;
+    
+    if(loginPwValue === "") {
+      alert("비밀번호를 입력해주세요.");
+    }
+    else {
+      getInfo();
+    }
+  }
+
+  function checkId(){
+    const loginId = document.querySelector(".login-id");
+    const loginIdValue = loginId.value;
+    
+    if(loginIdValue === "") {
+      alert("아이디를 입력해주세요.");
+    }
+    else {
+      checkPw();
+    }
+  }
+ 
   return(
     <>
       <div className="wrap lg_contents">
         <div className="container">
           <h3 className="title">Login</h3>
           <div className="lg-box">
-            <input type="text" placeholder="ID"/>
-            <input type="password" placeholder="PASSWORD"/>
+            <input type="text" placeholder="ID" className="login-id"/>
+            <input type="password" placeholder="PASSWORD" className="login-pw"/>
           </div>
 
           <div className="bt-box">
-            <Link to="/">
+            <button type="button" onClick={checkId} className="bt">
               Sign In
-            </Link>
-            <Link to="/join">
+            </button>
+            <Link to="/join" className="bt">
               Sign Up
             </Link>
           </div>
