a693696f9b992e65ab39ea122c34cc7ea33a2ed3
dev-93 <cjb940103@gmail.com>
2020-02-02 17:44:49 +0900

    기존 option들을 Phonenumber로 컴포넌트화

--- project/src/pages/Join/template/joincontents.js
+++ project/src/pages/Join/template/joincontents.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/joincontents.css";
+import PhoneNumber from "./joinnum";
 
 function JoinContents(){
   const USER_INFO = "userInfo";
@@ -138,12 +139,7 @@ function JoinContents(){
                 <th scope="row">핸드폰</th>
                 <td>
                   <select name="mobile_num" >
-                    <option value="010">010</option>
-                    <option value="011">011</option>
-                    <option value="016">016</option>
-                    <option value="017">017</option>
-                    <option value="018">018</option>
-                    <option value="019">019</option>
+                    <PhoneNumber />
                   </select>
                   -
                   <input maxLength="4" type="text" />
---
+++ project/src/pages/Join/template/joinnum.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PhoneNumber(){
+  return(
+  <>
+    <option value="010">010</option>
+    <option value="011">011</option>
+    <option value="016">016</option>
+    <option value="017">017</option>
+    <option value="018">018</option>
+    <option value="019">019</option>
+  </>
+  )
+}
