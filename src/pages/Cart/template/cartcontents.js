81f3fd3043a178727c2424cf95d3793569aa8e40
dev-93 <cjb940103@gmail.com>
2020-02-09 16:23:25 +0900

    장바구니(CART) 기능개발중, tab기능

--- src/pages/Cart/template/cartcontents.js
+++ src/pages/Cart/template/cartcontents.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../css/cartcontents.css"
+import CartList from "../../../components/cartlist/cartlist"
+
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+}
 
 function CartContents() {
+  const {currentItem ,changeItem} = useTabs(0, CartList);
+
   return (
     <>
       <div className="wrap ca_contents">
@@ -9,12 +21,13 @@ function CartContents() {
           <h3 className="title">CART</h3>
           <div className="container">
             <div className="tab_box">
-              <button type="button" className="tab domestic">국내배송상품(<span className="num">0</span>)</button>
-              <button type="button" className="tab foreign">해외배송상품(<span className="num">0</span>)</button>
+              {CartList.map((tabs, index) => 
+                (<button onClick={() => changeItem(index)} key={tabs.id}>{tabs.tab}</button>
+              ))}
             </div>
             
             <div className="cart_box">
-              <div className="empty_cart">장바구니가 비어 있습니다.</div>
+                <div className="empty_cart">{currentItem.content}</div>
             </div>
 
             <div className="bt_box">
