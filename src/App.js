82036454d767e5434028b1cf865d2eb15d8513fe
dev-93 <cjb940103@gmail.com>
2020-02-15 16:23:24 +0900

    App.js 코드정리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import './common/css/common.css';
 import './common/css/library.css';
+
+import Nav from "./pages/Nav/nav";
 import MainContainer from "./pages/Main/maincontainer";
 import LoginContainer from "./pages/Login/logincontainer";
 import JoinContainer from "./pages/Join/joincontainer";
 import CartContainer from "./pages/Cart/cartcontainer";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Nav from "./pages/Nav/nav";
 import ItemContents from './pages/Item/template/ltemcontents';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 class App extends Component {
