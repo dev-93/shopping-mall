4d84fb2652868a45f97dcfbd98d8f45860175c86
dev-93 <cjb940103@gmail.com>
2020-05-31 14:19:06 +0900

    Updates

---
+++ precache-manifest.0721e8916324221d017bb455e4c7bb29.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c137ee35c2914a642537183dae10a28",
+    "url": "/shopping-mall/index.html"
+  },
+  {
+    "revision": "12aef8bbee61eff923ce",
+    "url": "/shopping-mall/static/css/main.3c8eaec2.chunk.css"
+  },
+  {
+    "revision": "c880954f7bb6347478ca",
+    "url": "/shopping-mall/static/js/2.6843e926.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/shopping-mall/static/js/2.6843e926.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12aef8bbee61eff923ce",
+    "url": "/shopping-mall/static/js/main.7019f5fe.chunk.js"
+  },
+  {
+    "revision": "d7104eb27f658f331e12",
+    "url": "/shopping-mall/static/js/runtime-main.a7d10ca9.js"
+  },
+  {
+    "revision": "a5c59ebeefe8b9a53fa6f5a02368b575",
+    "url": "/shopping-mall/static/media/slider_test-1.a5c59ebe.jpg"
+  },
+  {
+    "revision": "7da7f19dc35bd9e60b37bfa89089586b",
+    "url": "/shopping-mall/static/media/slider_test-2.7da7f19d.jpg"
+  },
+  {
+    "revision": "c6eb7b9b8541404c9e21a589ac4f84b3",
+    "url": "/shopping-mall/static/media/slider_test-3.c6eb7b9b.jpg"
+  },
+  {
+    "revision": "ce3ddb09e06d3c05890ccdf965b67dfb",
+    "url": "/shopping-mall/static/media/slider_test-4.ce3ddb09.jpg"
+  },
+  {
+    "revision": "086078ad6007d5b9363ee5d30a0e81e6",
+    "url": "/shopping-mall/static/media/slider_test-5.086078ad.jpg"
+  },
+  {
+    "revision": "c1f19fd6ac91a0f11f0096bcf3f201dc",
+    "url": "/shopping-mall/static/media/slider_test-6.c1f19fd6.jpg"
+  }
+]);
